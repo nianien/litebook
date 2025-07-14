25909c53f55828159bfcbdbbafe8d71b97740a3e
nianien <nianien@gmail.com>
2025-07-14 11:28:08 +1000

    init

--- build.sh
+++
@@ -1,9 +0,0 @@
-#远程构建
-gcloud builds submit \
-  --tag asia-east1-docker.pkg.dev/nianien/liteblog/liteblog-app:latest .
-
-gcloud run deploy liteblog \
-  --image asia-east1-docker.pkg.dev/nianien/liteblog/liteblog-app:latest \
-  --platform managed \
-  --region asia-east1 \
-  --allow-unauthenticated
